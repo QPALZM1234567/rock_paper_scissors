32a2f3c061d75b11dc0808d9452dfc863f16022c
Sean <seanjung03@gmail.com>
2022-07-22 10:26:16 -0400

    Finish Rock Paper Scissor console game

---
+++ game.js
@@ -0,0 +1,46 @@
+function getComputerChoice() {
+    let number = Math.floor(Math.random() * 3);
+    if (number === 0) {
+        return "Rock";
+    } else if (number === 1) {
+        return "Scissors";
+    } else {
+        return "Paper";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
+        return "It's a tie! Both players played " + playerSelection.toUpperCase(); 
+    } else if (playerSelection.toUpperCase() === "ROCK") {
+        if (computerSelection.toUpperCase() === "PAPER") {
+            return "You lose! Paper beats Rock";
+        } else {
+            return "You win! Rock beats Scissors"
+        }
+    } else if (playerSelection.toUpperCase() === "PAPER") {
+        if (computerSelection.toUpperCase() === "ROCK") {
+            return "You win! Paper beats Rock";
+        } else {
+            return "You lose! Scissors beat Paper"
+        }
+    } else {
+        if (computerSelection.toUpperCase() === "PAPER") {
+            return "You win! Scissors beat Paper";
+        } else {
+            return "You lose! Rock beats Scissors"
+        }
+    }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("What is your play?");
+        let computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+
+game();
+
+
